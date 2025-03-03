e18a50977698089ed86ffcb16b37ab7c28884ab4
Sarffk17 <Ksarff17@gmail.com>
2025-03-02 20:28:45 -0500

    Created project and transferred data

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import axios from 'axios'
+import SearchBar from "./SearchBar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Weather App
+        </h1>
+        <SearchBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
